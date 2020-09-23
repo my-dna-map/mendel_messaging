532e38c208a142d733707a45699ac52ea4a857ed
marior <marior666@hotmail.com>
2020-09-23 12:25:36 +0200

    auth_token as static

--- src/MendelMessaging.js
+++ src/MendelMessaging.js
@@ -12,207 +12,207 @@ const amqp = require('amqplib');
  *
  */
 class MendelMessaging {
-  config = null;
-  source = null;
-  consume_connection = null;
-  consume_channel = null;
-  MQServer = "mq.mydnamap.com";
-  queueName = 'HPC';
+    config = null;
+    source = null;
+    consume_connection = null;
+    consume_channel = null;
+    MQServer = "mq.mydnamap.com";
+    queueName = 'HPC';
 
 
-  /**
-   *
-   * @param source
-   * @param config
-   */
-  constructor(source, config, queueName = null) {
-    this.source = source;
-    this.config = config ? config : require("config");
-    this.config = this.config.mq;
-    this.MQServer = this.config.MQServer;
-    if (!queueName) {
-      this.queueName = this.config.queueName;
-    } else {
-      this.queueName = queueName
+    /**
+     *
+     * @param source
+     * @param config
+     */
+    constructor(source, config, queueName = null) {
+        this.source = source;
+        this.config = config ? config : require("config");
+        this.config = this.config.mq;
+        this.MQServer = this.config.MQServer;
+        if (!queueName) {
+            this.queueName = this.config.queueName;
+        } else {
+            this.queueName = queueName
+        }
     }
-  }
 
-  /**
-   * Put an message into the message queue
-   * @param msg message to be posted on queue (message will be altered with tow new fields. event and source)
-   * @returns {Promise<void>}
-   */
-  emit(event, msg) {
-    return new Promise((resolve, reject) => {
-      msg.event = event;
-      msg.source = this.source;
-      msg.Id = uuid.v4();
+    /**
+     * Put an message into the message queue
+     * @param msg message to be posted on queue (message will be altered with tow new fields. event and source)
+     * @returns {Promise<void>}
+     */
+    emit(event, msg) {
+        return new Promise((resolve, reject) => {
+            msg.event = event;
+            msg.source = this.source;
+            msg.Id = uuid.v4();
+            let queueName = msg.queue ? msg.queue : this.queueName;
 
-      let open = amqp.connect(this.MQServer);
+            let open = amqp.connect(this.MQServer);
 
-      open.then((conn) => {
-        return conn.createChannel();
-      })
-          .then((ch) => {
-            return ch.assertQueue(this.queueName)
-                .then((ok) => {
-                  resolve(ch.sendToQueue(this.queueName, Buffer.from(JSON.stringify(msg))));
-                });
-          })
-          .catch(e => reject(e));
-    });
-  }
-
-  /**
-   * Subscribe to an specific queue
-   * @param callback callback function
-   * @param queue queue to subscribe
-   * @returns {Promise<void>}
-   */
-  async subscribeToQueue(queueName, callback) {
-
-    amqp.connect(this.MQServer)
-        .then((conn) => {
-          this.consume_connection = conn;
-
-          console.log(` ******   Connected to MQ ${this.MQServer} **********`);
-          conn.on('error', (err) => {
-            console.log("ERROR: %s", err);
-            conn.close();
-            setTimeout(function () {
-              //self.consume();
-            }, 50000);
-          });
-
-          conn.on("closed", () => {
-            console.log("Connection Closed");
-            setTimeout(function () {
-              //self.consume();
-            }, 50000);
-          });
-
-          conn.createChannel()
-              .then((ch) => {
-                this.consume_channel = ch;
-                var ok = ch.assertExchange(queueName, 'topic', {durable: false})
-                    .then(() => {
-                      return ch.assertQueue(queueName, {exclusive: false});
-                    })
-                    .then((qok) => {
-                      return ch.bindQueue(qok.queue, queueName, '')
-                          .then(function () {
-                            return qok.queue;
-                          });
-                    })
-                    .then((queue) => {
-                      ch.prefetch(1);
-                      ch.qos(1);
-                      ch.consume(queue, (msg) => {
-                        try {
-                          //self.messageReceived(JSON.parse(msg.content.toString()));
-                          callback(JSON.parse(msg.content.toString())).then(() => {
-                            ch.ack(msg);
-                          });
-                        } catch (ex) {
-                          console.error(ex);
-                        }
-                      }, {noAck: false});
-                    });
-              });
-        });
-  }
-
-
-  readMessageFromQueue(ch, queue, callback) {
-    if (ch) {
-      ch.get(queue)
-          .then(msg => {
-            // msg will be set to false if no messages are available on the queue.
-            if (msg) {
-              try {
-                callback(JSON.parse(msg.content.toString())).then(() => {
-                  ch.ack(msg);
-                  this.readMessageFromQueue(ch, queue, callback);
-                }).catch(e => {
-                  ch.nack(msg, false, true);
-                  setTimeout(() => {
-                    this.readMessageFromQueue(ch, queue, callback)
-                  }, 1000);
-                  logger.error(ex);
+            open.then((conn) => {
+                return conn.createChannel();
+            })
+                .then((ch) => {
+                    return ch.assertQueue(queueName)
+                        .then((ok) => {
+                            resolve(ch.sendToQueue(queueName, Buffer.from(JSON.stringify(msg))));
+                        });
                 })
-              } catch (ex) {
-                ch.nack(msg, false, true);
-                setTimeout(() => {
-                  this.readMessageFromQueue(ch, queue, callback)
-                }, 1000);
-                logger.error(ex);
-              }
-            } else {
-              setTimeout(() => {
-                this.readMessageFromQueue(ch, queue, callback)
-              }, 1000);
-            }
-          });
-
-    } else {
-      setTimeout(() => {
-        this.readMessageFromQueue(ch, queue, callback)
-      }, 1000);
+                .catch(e => reject(e));
+        });
     }
-  }
 
-  /**
-   *
-   * @param queueName
-   * @param callback
-   * @returns {Promise<void>}
-   */
-  async subscribeSingleMessageToQueue(queueName, callback, errfn) {
+    /**
+     * Subscribe to an specific queue
+     * @param callback callback function
+     * @param queue queue to subscribe
+     * @returns {Promise<void>}
+     */
+    async subscribeToQueue(queueName, callback) {
 
-    amqp.connect(this.MQServer)
-        .then((conn) => {
-          this.consume_connection = conn;
-          logger.info(` ******   Connected to MQ ${this.MQServer} **********`);
-          conn.on('error', (err) => {
-            console.log("ERROR: %s", err);
-            conn.close();
-            setTimeout(function () {
-              //self.consume();
-            }, 50000);
-          });
-          conn.on("closed", () => {
-            console.log("Connection Closed");
-            setTimeout(function () {
-              //self.consume();
-            }, 50000);
-          });
+        amqp.connect(this.MQServer)
+            .then((conn) => {
+                this.consume_connection = conn;
 
-          conn.createChannel()
-              .then((ch) => {
-                this.consume_channel = ch;
-                let ok = ch.assertExchange(queueName, 'topic', {durable: false})
-                    .then(() => {
-                      return ch.assertQueue(queueName, {exclusive: false});
-                    })
-                    .then((qok) => {
-                      return ch.bindQueue(qok.queue, queueName, '')
-                          .then(function () {
-                            return qok.queue;
-                          });
-                    })
-                    .then((queue) => {
+                console.log(` ******   Connected to MQ ${this.MQServer} **********`);
+                conn.on('error', (err) => {
+                    console.log("ERROR: %s", err);
+                    conn.close();
+                    setTimeout(function () {
+                        //self.consume();
+                    }, 50000);
+                });
 
-                      this.readMessageFromQueue(ch, queue, callback);
+                conn.on("closed", () => {
+                    console.log("Connection Closed");
+                    setTimeout(function () {
+                        //self.consume();
+                    }, 50000);
+                });
+
+                conn.createChannel()
+                    .then((ch) => {
+                        this.consume_channel = ch;
+                        var ok = ch.assertExchange(queueName, 'topic', {durable: false})
+                            .then(() => {
+                                return ch.assertQueue(queueName, {exclusive: false});
+                            })
+                            .then((qok) => {
+                                return ch.bindQueue(qok.queue, queueName, '')
+                                    .then(function () {
+                                        return qok.queue;
+                                    });
+                            })
+                            .then((queue) => {
+                                ch.prefetch(1);
+                                ch.qos(1);
+                                ch.consume(queue, (msg) => {
+                                    try {
+                                        //self.messageReceived(JSON.parse(msg.content.toString()));
+                                        callback(JSON.parse(msg.content.toString())).then(() => {
+                                            ch.ack(msg);
+                                        });
+                                    } catch (ex) {
+                                        console.error(ex);
+                                    }
+                                }, {noAck: false});
+                            });
                     });
-              });
-        })
-        .catch(ex => {
-          if (errfn) {
-            errfn(ex);
-          }
-        })
+            });
+    }
 
-  }
+
+    readMessageFromQueue(ch, queue, callback) {
+        if (ch) {
+            ch.get(queue)
+                .then(msg => {
+                    // msg will be set to false if no messages are available on the queue.
+                    if (msg) {
+                        try {
+                            callback(JSON.parse(msg.content.toString())).then(() => {
+                                ch.ack(msg);
+                                this.readMessageFromQueue(ch, queue, callback);
+                            }).catch(e => {
+                                ch.nack(msg, false, true);
+                                setTimeout(() => {
+                                    this.readMessageFromQueue(ch, queue, callback)
+                                }, 1000);
+                                logger.error(ex);
+                            })
+                        } catch (ex) {
+                            ch.nack(msg, false, true);
+                            setTimeout(() => {
+                                this.readMessageFromQueue(ch, queue, callback)
+                            }, 1000);
+                            logger.error(ex);
+                        }
+                    } else {
+                        setTimeout(() => {
+                            this.readMessageFromQueue(ch, queue, callback)
+                        }, 1000);
+                    }
+                });
+
+        } else {
+            setTimeout(() => {
+                this.readMessageFromQueue(ch, queue, callback)
+            }, 1000);
+        }
+    }
+
+    /**
+     *
+     * @param queueName
+     * @param callback
+     * @returns {Promise<void>}
+     */
+    async subscribeSingleMessageToQueue(queueName, callback, errfn) {
+
+        amqp.connect(this.MQServer)
+            .then((conn) => {
+                this.consume_connection = conn;
+                logger.info(` ******   Connected to MQ ${this.MQServer} **********`);
+                conn.on('error', (err) => {
+                    console.log("ERROR: %s", err);
+                    conn.close();
+                    setTimeout(function () {
+                        //self.consume();
+                    }, 50000);
+                });
+                conn.on("closed", () => {
+                    console.log("Connection Closed");
+                    setTimeout(function () {
+                        //self.consume();
+                    }, 50000);
+                });
+
+                conn.createChannel()
+                    .then((ch) => {
+                        this.consume_channel = ch;
+                        let ok = ch.assertExchange(queueName, 'topic', {durable: false})
+                            .then(() => {
+                                return ch.assertQueue(queueName, {exclusive: false});
+                            })
+                            .then((qok) => {
+                                return ch.bindQueue(qok.queue, queueName, '')
+                                    .then(function () {
+                                        return qok.queue;
+                                    });
+                            })
+                            .then((queue) => {
+                                this.readMessageFromQueue(ch, queue, callback);
+                            });
+                    });
+            })
+            .catch(ex => {
+                if (errfn) {
+                    errfn(ex);
+                }
+            })
+
+    }
 }
 
 
